0dae93228e3d9480c0b32969af07701d6d3a5d61
ice bear 32 <91836140+ice-bear-32@users.noreply.github.com>
2022-07-24 23:00:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-document.write("<script src=\"js/main/disableElementSourceHotKeys.js\"></script>");
+const scripts = document.querySelectorAll("script");
+const hasRouterScript = scripts.find(script => script.src.endsWith("js/managers/pages/pageRouter.js"));
+if(!hasRouterScript)
+{
+   const routerScript = document.createElement("script");
+   routerScript.src = "js/managers/pages/pageRouter.js";
+   document.querySelector("head").appendChild(routerScript);
+}
+
+const hasProtectionScript = scripts.find(script => script.src.endsWith("js/managers"));
