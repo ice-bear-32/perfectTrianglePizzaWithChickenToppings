5bcfe72fbea7dbf93b6978fdbed456430bb836c8
ice bear 32 <91836140+ice-bear-32@users.noreply.github.com>
2022-07-25 21:41:49 +0530

    Update index.html.js

--- js/managers/pages/index.html.js
+++ js/managers/pages/index.html.js
@@ -1,1 +1,8 @@
-document.write("test")
+fetch("page/page.index.html").then(res => {
+   return res.text();
+}).then(body => {
+   const html = document.createElement("iframe");
+   html.innerHTML = body;
+   html.classList.add("window-page-html");
+   document.querySelector("body").appendChild(html)
+});
