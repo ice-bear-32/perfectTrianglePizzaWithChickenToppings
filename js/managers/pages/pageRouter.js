9d530f5d5af16e1114bef672e9e7230f6544152c
ice bear 32 <91836140+ice-bear-32@users.noreply.github.com>
2022-07-25 21:15:43 +0530

    Update pageRouter.js

--- js/managers/pages/pageRouter.js
+++ js/managers/pages/pageRouter.js
@@ -7,7 +7,4 @@ const route = location.href.includes("#") ? location.href.split("#")[1] : "";
 const routeSrc = files.find(file => file.route == "#"+route).src;
 const routeFileScript = document.createElement("script");
 routeFileScript.src = routeSrc;
-document.querySelector("head").appendChild(routeFileScript);
-if(files.find(file => file.route == "#"+route).routeName == "index") {
-   indexRoute()
-}
+document.querySelector("body").appendChild(routeFileScript);
